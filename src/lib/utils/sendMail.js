fca0dfd7df341b36e9ae2697cad51d14f93b4410
truongluxury <truongbdhe172775@fpt.edu.vn>
2026-07-16 14:37:15 +0700

    fix: change nodemailer to explicitly use port 587 to avoid ETIMEDOUT on Render

--- src/lib/utils/sendMail.js
+++ src/lib/utils/sendMail.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
